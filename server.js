0d0f6040c60baa33bd5236d91c2fde96e764f317
skommireddi <30093161+skommireddi@users.noreply.github.com>
2019-05-16 16:14:29 -0700

    Adding node js files

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Poll Party Node JS Web App\n');
+}).listen(port);
