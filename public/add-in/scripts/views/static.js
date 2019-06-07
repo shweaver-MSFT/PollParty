7706c05885b6da03ff01a016c3c13ae1f0762a38
skommireddi <30093161+skommireddi@users.noreply.github.com>
2019-06-07 15:42:04 -0700

    refactored powerpointHelper, Hooking up powerpointhelper to static view

--- public/add-in/scripts/views/static.js
+++ public/add-in/scripts/views/static.js
@@ -1,10 +1,10 @@
-(function() {
+(function () {
     "use strict";
 
     let StaticView = function () {
 
         let initialize = function (view, data) {
-            
+
             let editButton = view.querySelector(".command-button");
             let questionTextSpan = view.querySelector(".question-text");
             let questionCountDiv = view.querySelector('.question-count');
@@ -22,11 +22,11 @@
 
                 // Configure question text
                 questionTextSpan.innerText = questionData.text;
-            
+
                 // Configure edit button
                 editButton.disabled = false;
-                editButton.addEventListener("click", function() {
-                    
+                editButton.addEventListener("click", function () {
+
                     // Navigate to the edit view
                     window.PollParty.App.navigate(window.PollParty.Views.EditView, {
                         question: questionData
@@ -43,19 +43,19 @@
             else {
                 // Attempt to retrieve the question from the backend 
                 // using the presentation id and slide id
-                let presentationId = 1;
-                let slideId = 1;
+                let presentationId = window.PollParty.Helpers.PowerPointHelper.getPresentationId();
+                let slideId = window.PollParty.Helpers.PowerPointHelper.getSelectedSlideId();
 
                 let url = `./api/question?pid=${presentationId}&sid=${slideId}`;
 
                 let xhr = new XMLHttpRequest();
                 xhr.responseType = "json";
                 xhr.open("GET", url);
-                xhr.addEventListener("load", function() {
+                xhr.addEventListener("load", function () {
                     if (xhr.status !== 200) {
                         return;
                     }
-                    
+
                     // Inspect json and populate the question object
                     questionData = xhr.response;
                     finishSetup();
