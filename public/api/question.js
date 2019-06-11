9408615998a07127edf1d27e2c278975f0588752
skommireddi <30093161+skommireddi@users.noreply.github.com>
2019-06-10 23:04:16 -0700

    Update question.js

--- public/api/question.js
+++ public/api/question.js
@@ -1,37 +1,52 @@
-// Question class exposing CRUD operations to add, read, edit, delete questions
-class question { 
-    
-    static getById(id){
-        return this.questions.find(item => item.id == id); 
-    }
-    
-    static get(presentationId, slideId){
-        return this.questions.find(item => item.presentationId === presentationId && item.slideId === slideId);
+// Question class to store questions and question count.
+class question {
+
+    static getById(id) {
+        return this.data.questions.find(item => item.id == id);
     }
 
-    static save(slideId, presentationId, text){
-        this.questions.push({id: this.id + 1, slideId: slideId, presentationId: presentationId, text: text });
+    static get(presentationId, slideId) {
+        return this.data.questions.find(item => item.presentationId === presentationId && item.slideId === slideId);
     }
 
-    static edit(id, slideId, presentationId, text){
-        var item = this.get(id);
-        item.slideId = slideId;
-        item.presentationId = presentationId;
-        item.text = text;
+    static save(presentationId, slideId, text, questionIndex) {
+        let item = this.get(presentationId, slideId);
+        if (item === null) {
+            // create new
+            this.data.questions.push({
+                id: slideId + presentationId,
+                slideId: slideId,
+                presentationId: presentationId,
+                text: text,
+                questionIndex: questionIndex
+            });
+        }
+        else {
+            //update
+            item.text = text;
+            item.questionIndex = questionIndex;
+        }
+        this.data.questionTotal = questionTotal + 1;
+        return { questionTotal: this.data.questionTotal, currentQuestion: item };
     }
-
-    static delete(id){
-        this.questions.pop(this.get(id));
-    }    
 }
 
 // Some mock data
-question.questions = [{
-    id: 1, 
-    slideId: 1, 
-    presentationId: 1, 
-    questionIndex: 1,
-    questionTotal: 1,
-    text: "Poll Party Hello World?"}];
+question.data = {
+    questionTotal: 0,
+    questions: []
+};
+
+// sample data
+// question.data = {
+//     questionTotal: 0,
+//     questions: [{
+//         id: null,
+//         slideId: 1,
+//         presentationId: 1,
+//         questionIndex: 1,
+//         text: "Poll Party Hello World?"
+//     }]
+// };
 
 module.exports = question;
